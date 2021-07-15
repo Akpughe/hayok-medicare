8c8e3d157fbd39b41e511cee415d9c0d412b26c3
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-15 04:00:27 +0100

    add new models

--- server/models/Encounter.js
+++ server/models/Encounter.js
@@ -1,44 +1,62 @@
 var mongoose = require('mongoose');
 
-var PatientVitalsSchema = new mongoose.Schema({
-  // patient: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Patient',
-  // },
-  patientId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Patient',
+var EncounterSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Patient',
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    visit: {
+      type: String,
+      required: true,
+      enum: ['First Time', 'Repeat Visit'],
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+    bmi: {
+      type: Number,
+      required: true,
+    },
+    bloodPressure: {
+      type: String,
+      required: true,
+    },
+    temperature: {
+      type: String,
+      required: true,
+    },
+    respiratoryRate: {
+      type: String,
+      required: true,
+    },
+    complain: {
+      type: String,
+      required: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    treatment: {
+      type: String,
+      required: true,
+    },
   },
-  date: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-  temperature: {
-    type: String,
-    required: true,
-  },
-  bloodPressure: {
-    type: String,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  height: {
-    type: Number,
-    required: true,
-  },
-  bodyMass: {
-    type: Number,
-    required: true,
-  },
-},
-    {timestamps:true}
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('PatientVitals', PatientVitalsSchema);
+module.exports = mongoose.model('Encounter', EncounterSchema);
