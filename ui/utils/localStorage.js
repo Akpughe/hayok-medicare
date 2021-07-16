cef425ef1f3d8609e9c68b16ec6c685c4dc9f4a5
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-16 15:38:35 +0100

    auth context

---
+++ ui/utils/localStorage.js
@@ -0,0 +1,31 @@
+const localStorageKey = 'hayok';
+// import setAuthToken from "./setAuthToken";
+export const getUser = () => {
+  try {
+    // const serializedUser = setAuthToken(localStorage.token);
+    const serializedUser = localStorage.getItem(localStorageKey);
+    if (serializedUser === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedUser);
+  } catch (e) {
+    throw new Error('Could not fetch user object from local storage');
+  }
+};
+
+export const saveUser = (user) => {
+  try {
+    const serializedUser = JSON.stringify(user);
+    localStorage.setItem(localStorageKey, serializedUser);
+  } catch (e) {
+    throw new Error('Could not save user object to local storage');
+  }
+};
+
+export const clearState = () => {
+  try {
+    localStorage.removeItem(localStorageKey);
+  } catch (e) {
+    throw new Error('Could not delete user object from local storage');
+  }
+};
