010ae91f9027216f1ac2ed6ccc07767d3d16ce39
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-16 15:41:32 +0100

    added progress bar

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-// import '../styles/tailwind.css'
+import '../styles/globals.css';
+import '../styles/tailwind.css'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Head from 'next/head'
+import { AuthProvider } from '../utils/authContext';
+
+const showProgressBar = (delay) => {
+  const timer = setTimeout(() => NProgress.start(), delay);
+  Router.events.on('routeChangeComplete', () => stopProgress(timer));
+  Router.events.on('routeChangeError', () => stopProgress(timer));
+};
+
+const stopProgress = (timer) => {
+  clearTimeout(timer);
+  NProgress.done();
+};
+
+Router.events.on('routeChangeStart', () => showProgressBar(300));
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="/css/nprogress.css" />
+        <title>Hayok Medicare</title>
+      </Head>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
