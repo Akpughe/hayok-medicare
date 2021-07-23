1a77228fc0fb97893fb3be0adabea5e51d4d04bb
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-23 01:26:56 +0100

    conditional rendering based on user type

--- ui/components/Sidebar.js
+++ ui/components/Sidebar.js
@@ -1,48 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { AuthContext } from '../utils/authContext';
 
 const Sidebar = () => {
+  const { user } = useContext(AuthContext);
+
+  user.user.department ? console.log('worker') : console.log('patient');
   const router = useRouter();
   return (
     <nav className="bg-white h-screen border border-r w-72 fixed pt-16 ">
       <div className="p-7">
         <div className="flex flex-col space-y-4">
           {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-          <Link href="/dashboard">
+          <Link href={user.user.department ? `/dashboard` : `/home`}>
             <a
               className={
-                (router.pathname === `/dashboard`
+                router.pathname === `/dashboard` || `/home`
                   ? `text-gray-500 border-l-4 border-blue-600 px-5 py-4 text-sm font-medium`
-                  : `text-gray-500 px-5 py-4 text-sm font-medium`)
+                  : `text-gray-500 px-5 py-4 text-sm font-medium`
               }
             >
-              Dashboard
+              {user.user.department ? `Dashboard` : `Home`}
             </a>
           </Link>
-          <Link href="/patient">
+          <Link href={user.user.department ? `/patient` : `#`}>
             <a
               className={
-                (router.pathname === `/patient`
+                router.pathname === `/patient`
                   ? `text-gray-500 border-l-4 border-blue-600 px-5 py-4 text-sm font-medium`
-                  : `text-gray-500 px-5 py-4 text-sm font-medium`)
+                  : `text-gray-500 px-5 py-4 text-sm font-medium`
               }
             >
               {' '}
-              View Patients
+              {user.user.department ? `View Patient` : ``}
             </a>
           </Link>
 
-          <Link href="/register">
+          <Link href={user.user.department ? `/register` : `#`}>
             <a
               className={
-                (router.pathname === `/register`
+                router.pathname === `/register`
                   ? `text-gray-500 border-l-4 border-blue-600 px-5 py-4 text-sm font-medium`
-                  : `text-gray-500 px-5 py-4 text-sm font-medium`)
+                  : `text-gray-500 px-5 py-4 text-sm font-medium`
               }
             >
               {' '}
-              Add Patient
+              {user.user.department ? `Add Patient` : ``}
             </a>
           </Link>
         </div>
